56c657b773f99f3379f64872a667ca62f1470520
Jiajun Yan <yanjiajun@ifanr.com>
2022-05-17 15:36:43 +0800

    feat: 添加 eslint 和 prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,45 @@
-/*
- * Eslint config file
- * Documentation: https://eslint.org/docs/user-guide/configuring/
- * Install the Eslint extension before using this feature.
- */
 module.exports = {
+  root: true,
   env: {
-    es6: true,
     browser: true,
-    node: true,
-  },
-  ecmaFeatures: {
-    modules: true,
+    commonjs: true,
+    es6: true,
   },
   parserOptions: {
-    ecmaVersion: 2018,
-    sourceType: 'module',
+    ecmaVersion: 2019, // Allows for the parsing of mode`rn ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
   },
+  parser: 'babel-eslint',
+  extends: ['standard', 'plugin:prettier/recommended', 'plugin:import/recommended'],
   globals: {
-    wx: true,
     App: true,
-    Page: true,
-    getCurrentPages: true,
-    getApp: true,
+    BaaS: true,
+    Behavior: true,
     Component: true,
+    EventChannel: true,
+    getApp: true,
+    getCurrentPages: true,
+    Page: true,
     requirePlugin: true,
-    requireMiniProgram: true,
+    wx: true,
   },
-  // extends: 'eslint:recommended',
-  rules: {},
+  rules: {
+    'prettier/prettier': [
+      2,
+      {
+        bracketSpacing: false,
+        printWidth: 120,
+        semi: false,
+        singleQuote: true,
+        tabWidth: 2,
+        trailingComma: 'es5',
+      },
+    ],
+    'handle-callback-err': 0,
+    'prefer-promise-reject-errors': [2, {allowEmptyReject: true}],
+    'no-prototype-builtins': 0,
+    'comma-dangle': 0,
+    camelcase: 0,
+    eqeqeq: 0,
+  },
 }
